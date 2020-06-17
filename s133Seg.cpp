0e96c7808c49956446a464399bc85aa45e4e64a7
giovanadanieles <giovana.daniele@usp.br>
2020-06-17 13:13:56 -0300

    Added: 133% shots

---
+++ s133Seg.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 4;
+
+void writeNewShots(const pair<string, string> shot[N], ofstream &outputFile, int count){
+	int fir[N], sec[N], range[N];
+
+	for(int i = 0; i < count; i++){
+		fir[i] = atoi(shot[i].first.c_str());
+		sec[i] = atoi(shot[i].second.c_str());
+	}
+
+	if(count == 1){
+		outputFile << fir[0] << "\t" << sec[0] << endl;
+	}
+	else if(count == 2){
+		range[0] = floor((sec[1] - fir[1]) * 33 / 100);
+
+		if(range[0] > 25){
+			outputFile << fir[0] << "\t" << sec[0] + range[0] << endl;
+			outputFile << sec[0] + range[0] + 1 << "\t" << sec[1] << endl;
+		}
+		else{
+			outputFile << fir[0] << "\t" << sec[0] << endl;
+			outputFile << fir[1] << "\t" << sec[1] << endl;
+		}
+	}
+	else if(count == 3){
+		range[0] = floor((sec[1] - fir[1]) * 33 / 100);
+		range[1] = floor((sec[2] - fir[2]) * 66 / 100);
+
+		if(range[0] > 25 && range[1] > 25){
+			outputFile << fir[0] << "\t" << sec[0] + range[0] << endl;
+
+			outputFile << sec[0] + range[0] + 1 << "\t" <<
+						  sec[0] + range[0] + 1 + range[1] << endl;
+
+			outputFile << sec[0] + range[0] + 2 + range[1] << sec[2] << endl;
+		}
+		else{
+			outputFile << fir[0] << "\t" << sec[0] << endl;
+			outputFile << fir[1] << "\t" << sec[1] << endl;
+			outputFile << fir[2] << "\t" << sec[2] << endl;
+		}
+	}
+	else if(count == 4){
+		range[0] = floor((sec[1] - fir[1]) * 33 / 100);
+		range[1] = floor((sec[2] - fir[2]) * 66 / 100);
+
+		if(range[0] > 25 && range[1] > 25){
+			outputFile << fir[0] << "\t" << sec[0] + range[0] << endl;
+
+			outputFile << sec[0] + range[0] + 1 << "\t" <<
+						  sec[0] + range[0] + 1 + range[1] << endl;
+
+			outputFile << sec[0] + range[0] + 2 + range[1] << "\t" << sec[3] << endl;
+		}
+		else{
+			outputFile << fir[0] << "\t" << sec[0] << endl;
+			outputFile << fir[1] << "\t" << sec[1] << endl;
+			outputFile << fir[2] << "\t" << sec[2] << endl;
+			outputFile << fir[3] << "\t" << sec[3] << endl;
+		}
+	}
+}
+
+void readOriginalShots(ifstream &inputFile, ofstream &outputFile){
+	string line;
+	int pos, count;
+	pair <string, string> shot[N];
+
+	while(getline(inputFile, line)){
+
+		pos = line.find_first_of('\t');
+		shot[0].first = line.substr(0, pos);
+		shot[0].second = line.substr(pos+1);
+
+		count = 1;
+
+		for(int i = 1; i < N; i++){
+			if(getline(inputFile, line)){
+				pos = line.find_first_of('\t');
+				shot[i].first = line.substr(0, pos);
+				shot[i].second = line.substr(pos+1);
+
+				count++;
+			}
+		}
+
+		writeNewShots(shot, outputFile, count);
+	}
+}
+
+int main(){
+	string filename; cin >> filename;
+
+	ifstream inputFile( filename.c_str() );
+
+	if(!inputFile){
+		cout << "Error" << endl;
+
+		return -1;
+	}
+	else{
+        ofstream outputFile("fragShots.csv");
+
+		readOriginalShots(inputFile, outputFile);
+
+		inputFile.close();
+		outputFile.close();
+	}
+
+	return 0;
+}
